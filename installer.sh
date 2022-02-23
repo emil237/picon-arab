2ad067bc97fe730a5292a74546c0e65ca2765525
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-23 21:07:24 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ sleep 1
 curl  -k -Lbk -m 55532 -m 555104  "https://drive.google.com/uc?id=1TmiCD_EH55CEp33r0ShZD3RzIYwtGvGm&export=download" > /tmp/test/picon-arab.zip
 sleep 1
 unzip /tmp/test/picon-arab.zip -d /medid/hdd/picon/
-chmod 644 /media/hdd/picon/picon
+chmod 644 /media/hdd/picon
 sleep 1
 cd /
 rm -rf /tmp/test
@@ -20,6 +20,8 @@ echo "PLEASE RESTARTING INIGMA2!!!!"
 sleep 2
 echo ""
 exit
+
+
 
 
 

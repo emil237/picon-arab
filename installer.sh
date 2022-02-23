70ae1ed6baa4ac5e9f3e2470a1e93c9bcd967e07
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-23 21:03:52 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -3,7 +3,6 @@ sleep 1
 echo "install PICON ... "
 echo ""
 sleep 1
-echo "Pozor!!! Pot?ebn? m?sto.... 5Mb"
 [ -d /tmp/test ] || mkdir -p /tmp/test
 curl  -k -Lbk -m 55532 -m 555104  "https://drive.google.com/uc?id=1TmiCD_EH55CEp33r0ShZD3RzIYwtGvGm&export=download" > /tmp/test/picon-arab.zip
 sleep 1
@@ -21,5 +20,6 @@ echo "PLEASE RESTARTING INIGMA2!!!!"
 sleep 2
 echo ""
 exit
+
 
 

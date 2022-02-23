0fcd813c6ba358b0f6736164f787a51939ae1da4
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-23 21:02:26 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,76 +1,25 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emil237/picon-arab/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL="https://raw.githubusercontent.com/emil237/picon-arab/main"  
-echo "******************************************************************************************************************"
-echo "    download and install picon  "
-echo "============================================================================================================================="
-#####################################################################################
-echo "         install picon-arab    "
-cd /tmp
-set -e 
-      curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/picon-arab/raw/main/picon1.tar.gz" > /tmp/picon1.tar.gz
-wait
-tar -xzf picon1.tar.gz -C /
-wait
-cd ..
-set +e
-rm -f /tmp/picon1.tar.gz
-echo "==========================================================================================================================="
-echo "         install picon-arab    "
-cd /tmp
-set -e 
-     curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/picon-arab/raw/main/picon2.tar.gz" > /tmp/picon2.tar.gz
-wait
-tar -xzf picon2.tar.gz -C /
-wait
-cd ..
-set +e
-rm -f /tmp/picon2.tar.gz
-sleep 2;
-echo "" 
-echo "" 
-echo "****************************************************************************************************************************"
-echo "# PICON  INSTALLED SUCCESSFULLY #"
-echo "
-echo " "*********************************************************" 
-	echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
-sleep 4;
-	echo '========================================================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
-echo "**********************************************************************************"
-wait
-killall -9 enigma2
-exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#!/bin/sh
+sleep 1
+echo "install PICON ... "
+echo ""
+sleep 1
+echo "Pozor!!! Pot?ebn? m?sto.... 5Mb"
+[ -d /tmp/test ] || mkdir -p /tmp/test
+curl  -k -Lbk -m 55532 -m 555104  "https://drive.google.com/uc?id=1TmiCD_EH55CEp33r0ShZD3RzIYwtGvGm&export=download" > /tmp/test/picon-arab.zip
+sleep 1
+unzip /tmp/test/picon-arab.zip -d /medid/hdd/picon/
+chmod 644 /media/hdd/picon/picon
+sleep 1
+cd /
+rm -rf /tmp/test
+echo ""
+sleep 1
+echo "OK"
+sleep 2
+echo ""
+echo "PLEASE RESTARTING INIGMA2!!!!"
+sleep 2
+echo ""
+exit
 
 

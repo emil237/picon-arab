e204be6c831b260f4cfc6054ec35a061bb7719f8
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-24 07:08:44 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #!/bin/sh # 
  # # Command: wget https://raw.githubusercontent.com/emil237/picon-arab/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL="https://raw.githubusercontent.com/emil237/picon-arab/main"  
+MY_URL="https://raw.githubusercontent.com/emil237/picon-arab/main"
+MY_PICON1="picon1.tar.gz"
+MY_PICON2="picon2.tar.gz"
 echo "******************************************************************************************************************"
 echo "    download and install picon  "
 echo "============================================================================================================================="
@@ -8,20 +10,20 @@ echo "==========================================================================
 echo "         install picon-arab    "
 cd /tmp
 set -e 
-      curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/picon-arab/raw/main/picon1.tar.gz" > /tmp/picon1.tar.gz
+      wget -q  "https://raw.githubusercontent.com/emil237/picon-arab/main/picon1.tar.gz"
 wait
-tar -xzf picon1.tar.gz -C /
+tar -xzf picon1.tar.gz  -C /
 wait
 cd ..
 set +e
 rm -f /tmp/picon1.tar.gz
 echo "==========================================================================================================================="
-echo "         install picon-arab    "
+echo "         install picon-arab   "
 cd /tmp
 set -e 
-     curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/picon-arab/raw/main/picon2.tar.gz" > /tmp/picon2.tar.gz
+       wget -q "https://raw.githubusercontent.com/emil237/picon-arab/main/picon2.tar.gz"
 wait
-tar -xzf picon2.tar.gz -C /
+tar -xzf picon2.tar.gz  -C /
 wait
 cd ..
 set +e
@@ -43,6 +45,11 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
+
+
+
+
 
 
 
